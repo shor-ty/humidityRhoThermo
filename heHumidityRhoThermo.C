9ed51c2ac119796659f89a747f63053aafe4c615
Tobias Holzmann <Tobias.Holzmann@Holzmann-cfd.de>
2018-10-14 18:23:19 +0200

    First commit

---
+++ heHumidityRhoThermo.C
@@ -0,0 +1,310 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "heHumidityRhoThermo.H"
+#include "fvMatricesFwd.H"
+//#include "surfaceFieldsFwd.H"
+//#include "primitiveFieldsFwd.H"
+#include "fvCFD.H"
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+template<class BasicPsiThermo, class MixtureType>
+void Foam::heHumidityRhoThermo<BasicPsiThermo, MixtureType>::calculate()
+{
+    const scalarField& hCells = this->he();
+    const scalarField& pCells = this->p_;
+
+    scalarField& TCells = this->T_.primitiveFieldRef();
+    scalarField& psiCells = this->psi_.primitiveFieldRef();
+    scalarField& rhoCells = this->rho_.primitiveFieldRef();
+    scalarField& muCells = this->mu_.primitiveFieldRef();
+    scalarField& alphaCells = this->alpha_.primitiveFieldRef();
+
+    forAll(TCells, celli)
+    {
+        const typename MixtureType::thermoType& mixture_ =
+            this->cellMixture(celli);
+
+        TCells[celli] = mixture_.THE
+        (
+            hCells[celli],
+            pCells[celli],
+            TCells[celli]
+        );
+
+        psiCells[celli] = mixture_.psi(pCells[celli], TCells[celli]);
+        rhoCells[celli] = mixture_.rho(pCells[celli], TCells[celli]);
+
+        muCells[celli] = mixture_.mu(pCells[celli], TCells[celli]);
+        alphaCells[celli] = mixture_.alphah(pCells[celli], TCells[celli]);
+    }
+
+    volScalarField::Boundary& pBf =
+        this->p_.boundaryFieldRef();
+
+    volScalarField::Boundary& TBf =
+        this->T_.boundaryFieldRef();
+
+    volScalarField::Boundary& psiBf =
+        this->psi_.boundaryFieldRef();
+
+    volScalarField::Boundary& rhoBf =
+        this->rho_.boundaryFieldRef();
+
+    volScalarField::Boundary& heBf =
+        this->he().boundaryFieldRef();
+
+    volScalarField::Boundary& muBf =
+        this->mu_.boundaryFieldRef();
+
+    volScalarField::Boundary& alphaBf =
+        this->alpha_.boundaryFieldRef();
+
+    forAll(this->T_.boundaryField(), patchi)
+    {
+        fvPatchScalarField& pp = pBf[patchi];
+        fvPatchScalarField& pT = TBf[patchi];
+        fvPatchScalarField& ppsi = psiBf[patchi];
+        fvPatchScalarField& prho = rhoBf[patchi];
+        fvPatchScalarField& phe = heBf[patchi];
+        fvPatchScalarField& pmu = muBf[patchi];
+        fvPatchScalarField& palpha = alphaBf[patchi];
+
+        if (pT.fixesValue())
+        {
+            forAll(pT, facei)
+            {
+                const typename MixtureType::thermoType& mixture_ =
+                    this->patchFaceMixture(patchi, facei);
+
+                phe[facei] = mixture_.HE(pp[facei], pT[facei]);
+
+                ppsi[facei] = mixture_.psi(pp[facei], pT[facei]);
+                prho[facei] = mixture_.rho(pp[facei], pT[facei]);
+                pmu[facei] = mixture_.mu(pp[facei], pT[facei]);
+                palpha[facei] = mixture_.alphah(pp[facei], pT[facei]);
+            }
+        }
+        else
+        {
+            forAll(pT, facei)
+            {
+                const typename MixtureType::thermoType& mixture_ =
+                    this->patchFaceMixture(patchi, facei);
+
+                pT[facei] = mixture_.THE(phe[facei], pp[facei], pT[facei]);
+
+                ppsi[facei] = mixture_.psi(pp[facei], pT[facei]);
+                prho[facei] = mixture_.rho(pp[facei], pT[facei]);
+                pmu[facei] = mixture_.mu(pp[facei], pT[facei]);
+                palpha[facei] = mixture_.alphah(pp[facei], pT[facei]);
+            }
+        }
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+template<class BasicPsiThermo, class MixtureType>
+Foam::heHumidityRhoThermo<BasicPsiThermo, MixtureType>::heHumidityRhoThermo
+(
+    const fvMesh& mesh,
+    const word& phaseName
+)
+:
+    heThermo<BasicPsiThermo, MixtureType>(mesh, phaseName)
+{
+    calculate();
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+template<class BasicPsiThermo, class MixtureType>
+Foam::heHumidityRhoThermo<BasicPsiThermo, MixtureType>::~heHumidityRhoThermo()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+template<class BasicPsiThermo, class MixtureType>
+void Foam::heHumidityRhoThermo<BasicPsiThermo, MixtureType>::correct()
+{
+    if (debug)
+    {
+        InfoInFunction << endl;
+    }
+
+    calculate();
+
+    pSatH2O();
+    partialPressureH2O();
+    absHumidity();
+    massH2O();
+    H2OTransport();
+
+    if (debug)
+    {
+        Info<< "    Finished" << endl;
+    }
+}
+
+
+template<class BasicPsiThermo, class MixtureType>
+void Foam::heHumidityRhoThermo<BasicPsiThermo, MixtureType>::pSatH2O()
+{
+    const volScalarField theta =
+        (this->T_
+      - dimensionedScalar("Kelvin", dimensionSet(0,0,0,1,0,0,0), scalar(273.15)))
+      * dimensionedScalar("perK", dimensionSet(0,0,0,-1,0,0,0), scalar(1));
+
+    //- Magnus formulation
+    //  Valid between -50 to 100 degC and 1013.25 hPa
+    if (this->method_ == "magnus")
+    {
+        dimensionedScalar pre1
+        (
+            "pre1",
+            dimensionSet(1,-1,-2,0,0,0,0),
+            scalar(611.2)
+        );
+
+        dimensionedScalar pre2
+        (
+            "pre2",
+            dimensionSet(0,0,0,0,0,0,0),
+            scalar(17.62)
+        );
+
+        dimensionedScalar value1
+        (
+            "value1",
+            dimensionSet(0,0,0,0,0,0,0),
+            scalar(243.12)
+        );
+
+        this->pSatH2O_ = pre1*exp((pre2*(theta))/(value1+theta));
+    }
+    //  Buck formula [1996]
+    //  Valid between 0 to 100 degC and 1013.25 hPa
+    //  Very accurate between 0 degC and 50 degC
+    else if (this->method_ == "buck")
+    {
+        dimensionedScalar pre1
+        (
+            "pre1",
+            dimensionSet(1,-1,-2,0,0,0,0),
+            scalar(611.21)
+        );
+
+        dimensionedScalar value1
+        (
+            "value1",
+            dimensionSet(0,0,0,0,0,0,0),
+            scalar(18.678)
+        );
+
+        dimensionedScalar value2
+        (
+            "value2",
+            dimensionSet(0,0,0,0,0,0,0),
+            scalar(234.5)
+        );
+
+        dimensionedScalar value3
+        (
+            "value3",
+            dimensionSet(0,0,0,0,0,0,0),
+            scalar(257.14)
+        );
+
+        this->pSatH2O_ =
+            pre1*exp(((value1-theta)/value2)*theta/(value3+theta));
+    }
+}
+
+
+template<class BasicPsiThermo, class MixtureType>
+void Foam::heHumidityRhoThermo<BasicPsiThermo, MixtureType>::partialPressureH2O()
+{
+    this->partialPressureH2O_ = this->relHum_ * this->pSatH2O_;
+}
+
+
+template<class BasicPsiThermo, class MixtureType>
+void Foam::heHumidityRhoThermo<BasicPsiThermo, MixtureType>::absHumidity()
+{
+    dimensionedScalar RSpecificH2O
+    (
+        "gasConstantH2O",
+        dimensionSet(0,2,-2,-1,0,0,0),
+        scalar(461.51)
+    );
+
+    this->absHum_ = this->partialPressureH2O_ / (this->T_ * RSpecificH2O);
+}
+
+
+template<class BasicPsiThermo, class MixtureType>
+void Foam::heHumidityRhoThermo<BasicPsiThermo, MixtureType>::massH2O()
+{
+    const scalarField& absHumidityCelli = this->absHum_;
+    scalarField& massH2OCelli = this->massH2O_.primitiveFieldRef();
+    const scalarField& volume = this->massH2O_.mesh().V();
+
+    forAll(massH2OCelli, cellI)
+    {
+        massH2OCelli[cellI] = absHumidityCelli[cellI]*volume[cellI]*1000;
+    }
+}
+
+
+template<class BasicPsiThermo, class MixtureType>
+void Foam::heHumidityRhoThermo<BasicPsiThermo, MixtureType>::H2OTransport()
+{
+    volScalarField& mH2O = this->massH2O_;
+    const surfaceScalarField& phi =
+        mH2O.mesh().lookupObject<surfaceScalarField>("phi");
+
+    //- Incompressible or compressible
+    bool compressible = true;
+    {
+        const objectRegistry& oR = this->db();
+        Info<< oR << endl;
+    }
+
+    /*
+    tmp<fvScalarMatrix> mH2OEqn
+    (
+        fvm::div(phi, mH2O)
+    );
+    */
+}
+
+
+
+// ************************************************************************* //
---
+++ humidityRhoThermo.C
@@ -0,0 +1,345 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2017 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "humidityRhoThermo.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(humidityRhoThermo, 0);
+    defineRunTimeSelectionTable(humidityRhoThermo, fvMesh);
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::humidityRhoThermo::humidityRhoThermo(const fvMesh& mesh, const word& phaseName)
+:
+    fluidThermo(mesh, phaseName),
+    rho_
+    (
+        IOobject
+        (
+            phasePropertyName("thermo:rho"),
+            mesh.time().timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh,
+        dimDensity
+    ),
+
+    psi_
+    (
+        IOobject
+        (
+            phasePropertyName("thermo:psi"),
+            mesh.time().timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh,
+        dimensionSet(0, -2, 2, 0, 0)
+    ),
+
+    mu_
+    (
+        IOobject
+        (
+            phasePropertyName("thermo:mu"),
+            mesh.time().timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh,
+        dimensionSet(1, -1, -1, 0, 0)
+    ),
+
+    relHum_
+    (
+        IOobject
+        (
+            phasePropertyName("thermo:relHum"),
+            mesh.time().timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh
+    ),
+
+    absHum_
+    (
+        IOobject
+        (
+            phasePropertyName("thermo:absHum"),
+            mesh.time().timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh,
+        dimDensity
+    ),
+
+    massH2O_
+    (
+        IOobject
+        (
+            phasePropertyName("thermo:massH2O"),
+            mesh.time().timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh,
+        dimMass
+    ),
+
+    pSatH2O_
+    (
+        IOobject
+        (
+            phasePropertyName("thermo:pSatH2O"),
+            mesh.time().timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh,
+        dimPressure
+    ),
+
+    partialPressureH2O_
+    (
+        IOobject
+        (
+            phasePropertyName("thermo:partialPressureH2O"),
+            mesh.time().timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh,
+        dimPressure
+    )
+
+{
+    method_ = word("buck");
+}
+
+
+Foam::humidityRhoThermo::humidityRhoThermo
+(
+    const fvMesh& mesh,
+    const dictionary& dict,
+    const word& phaseName
+)
+:
+    fluidThermo(mesh, dict, phaseName),
+    rho_
+    (
+        IOobject
+        (
+            phasePropertyName("thermo:rho"),
+            mesh.time().timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh,
+        dimDensity
+    ),
+
+    psi_
+    (
+        IOobject
+        (
+            phasePropertyName("thermo:psi"),
+            mesh.time().timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh,
+        dimensionSet(0, -2, 2, 0, 0)
+    ),
+
+    mu_
+    (
+        IOobject
+        (
+            phasePropertyName("thermo:mu"),
+            mesh.time().timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh,
+        dimensionSet(1, -1, -1, 0, 0)
+    ),
+
+    relHum_
+    (
+        IOobject
+        (
+            phasePropertyName("thermo:relHum"),
+            mesh.time().timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh,
+        dimensionSet(0, 0, 0, 0, 0)
+    ),
+
+    absHum_
+    (
+        IOobject
+        (
+            phasePropertyName("thermo:absHum"),
+            mesh.time().timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh,
+        dimDensity
+    ),
+
+    massH2O_
+    (
+        IOobject
+        (
+            phasePropertyName("thermo:massH2O"),
+            mesh.time().timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh,
+        dimMass
+    ),
+
+
+    pSatH2O_
+    (
+        IOobject
+        (
+            phasePropertyName("thermo:pSatH2O"),
+            mesh.time().timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh,
+        dimPressure
+    ),
+
+    partialPressureH2O_
+    (
+        IOobject
+        (
+            phasePropertyName("thermo:partialPressureH2O"),
+            mesh.time().timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh,
+        dimPressure
+    )
+{
+    method_ = word("buck");
+}
+
+
+// * * * * * * * * * * * * * * * * Selectors * * * * * * * * * * * * * * * * //
+
+Foam::autoPtr<Foam::humidityRhoThermo> Foam::humidityRhoThermo::New
+(
+    const fvMesh& mesh,
+    const word& phaseName
+)
+{
+    return basicThermo::New<humidityRhoThermo>(mesh, phaseName);
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::humidityRhoThermo::~humidityRhoThermo()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::volScalarField> Foam::humidityRhoThermo::rho() const
+{
+    return rho_;
+}
+
+
+Foam::tmp<Foam::scalarField> Foam::humidityRhoThermo::rho(const label patchi) const
+{
+    return rho_.boundaryField()[patchi];
+}
+
+
+Foam::volScalarField& Foam::humidityRhoThermo::rho()
+{
+    return rho_;
+}
+
+
+void Foam::humidityRhoThermo::correctRho(const Foam::volScalarField& deltaRho)
+{
+    rho_ += deltaRho;
+}
+
+
+const Foam::volScalarField& Foam::humidityRhoThermo::psi() const
+{
+    return psi_;
+}
+
+
+Foam::tmp<Foam::volScalarField> Foam::humidityRhoThermo::mu() const
+{
+    return mu_;
+}
+
+
+Foam::tmp<Foam::scalarField> Foam::humidityRhoThermo::mu(const label patchi) const
+{
+    return mu_.boundaryField()[patchi];
+}
+
+
+// ************************************************************************* //
